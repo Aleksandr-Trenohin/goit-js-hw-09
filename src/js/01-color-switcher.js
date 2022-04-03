971b75f8f96ed663d2536b6c445140aefdd8b4e2
Aleksandr-Trenohin <aleksandr_trenohin@bigmir.net>
2022-04-03 20:21:26 +0300

    add task 1 solution

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const bodyEl = document.body;
+
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+btnStop.setAttribute("disabled", "");
+
+btnStart.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    bodyEl.style = `background-color: ${getRandomHexColor()};`;
+  }, 1000);
+    btnStart.setAttribute("disabled", "");
+    btnStop.removeAttribute("disabled");
+});
+
+btnStop.addEventListener("click", () => {
+    clearInterval(timerId);
+    btnStop.setAttribute("disabled", "");
+    btnStart.removeAttribute("disabled");
+});
+
 
